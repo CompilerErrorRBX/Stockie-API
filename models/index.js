00a9c3b44f98dc020f555baeca30b246778ab4a3
Stephen Martin <stephen.martin@cerner.com>
2018-03-21 20:27:35 -0500

    Require environment variables for db user and pass

--- models/index.js
+++ models/index.js
@@ -9,6 +9,9 @@ const env       = process.env.NODE_ENV || 'development';
 const config    = require(__dirname + '/../config/config.json')[env];
 const db        = {};
 
+config.username = process.env[`STOCKIE_${env.toLocaleUpperCase()}_USERNAME`];
+config.password = process.env[`STOCKIE_${env.toLocaleUpperCase()}_PASSWORD`];
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
